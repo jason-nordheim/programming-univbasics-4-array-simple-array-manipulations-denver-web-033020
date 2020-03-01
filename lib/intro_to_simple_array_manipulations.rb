f255ec65949ac88efe9bf6ee3ebfd9f2acb4e669
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-01 18:00:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,15 @@ end
 def pop_with_args(array)
   return array.pop(2)
 end
+
+def using_shift(array)
+  return array.shift()
+end 
+
+def shift_with_args(array)
+  return array.shift(2) 
+end 
+
+# This method takes in two parameters of two different arrays 
+# and uses the .concat method to add the contents of the second array to the first.
+def using_concat(a1, a2)
