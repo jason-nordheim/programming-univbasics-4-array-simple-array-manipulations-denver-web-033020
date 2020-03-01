7629a77c39cdcda19222ddd59cbe0a9bbf8a5ab8
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-01 18:01:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,5 @@
 # This method takes in two parameters, an Array and a String.
 # It adds that string to the end of the array using the .push method.
-
-
-
 def using_push(array, string)
   return array.push(string)
 end
