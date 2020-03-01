b3624660b306dcada9f7ca52aae18102b89cedb8
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-01 18:00:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,10 @@ def using_pop(array)
   return array.pop()
 end
 
+# This method takes in an parameter of an Array and uses
+# the .pop method with an parameter of 2 to remove the last two array items and return them.
+
+
 def pop_with_args(array)
   return array.pop(2)
 end
